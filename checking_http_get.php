af5866c0547069583635119dc7a91eefcef9afd2
MegaFlummis <41554207+MegaFlummis@users.noreply.github.com>
2024-05-10 19:35:34 +0200

    Update checking_http_get.php

--- checking_http_get.php
+++ checking_http_get.php
@@ -1,7 +1,7 @@
 <?php
 // This is a script example for NFC Tools conditional block : HTTP GET
 // Add your conditional block like this : 
-// Your request : http//mydomain.tld/checking_http_get.php?id=5
+// Your request : http//narkotikatipset.se.tld/checking_http_get.php?id=5
 // Return value : ok
 
 if (isset($_GET["id"]) && $_GET["id"] == 5){
